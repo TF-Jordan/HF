0360e5cbfb01b9fb6443f3b785dd81d92e135479
ToulepiFoudjoJordan <jordantoulepi@gmail.com>
2026-02-16 17:37:04 +0100

    correction des bugs

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -47,7 +47,7 @@ abstract final class AppTheme {
         ),
         iconTheme: IconThemeData(color: c.textPrimary),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: c.card,
         elevation: isDark ? 0 : 2,
         shadowColor: isDark ? Colors.transparent : Colors.black12,
